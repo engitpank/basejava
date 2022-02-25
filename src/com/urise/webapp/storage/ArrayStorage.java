ff089f9ac7860110ce5839defddeded1875b17c5
engitpank <engutpunk@gmail.com>
2022-02-25 15:32:42 +0300

    Added hierarchical package structure -> The access modified is defined for all fields and methods of the classes -> Resume.uuid - added getter and setter -> ArrayStorage.delete() has been rewritten

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -1,37 +1,41 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.model.Resume;
+
 import java.util.Arrays;
 
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
-    int size = 0;
+    private final Resume[] storage = new Resume[10000];
+    private int size = 0;
 
-    void clear() {
+    public void clear() {
         Arrays.fill(storage, 0, size, null);
         size = 0;
     }
 
-    void save(Resume r) {
+    public void save(Resume r) {
         if (size < storage.length) {
             storage[size] = r;
             size++;
         }
     }
 
-    Resume get(String uuid) {
+    public Resume get(String uuid) {
         for (int i = 0; i < size; i++) {
-            if (storage[i].uuid.equals(uuid))
+            if (storage[i].getUuid().equals(uuid))
                 return storage[i];
         }
         return null;
     }
 
-    void delete(String uuid) {
-        for(int i = 0; i < size; i++){
-            if (storage[i].uuid.equals(uuid)){
-                storage[i] = storage[size-1];
-                storage[size-1] = null;
+    public void delete(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].getUuid().equals(uuid)) {
+                storage[i] = storage[size - 1];
+                storage[size - 1] = null;
                 size--;
             }
         }
@@ -40,14 +44,14 @@ public class ArrayStorage {
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
+    public Resume[] getAll() {
         Resume[] allResume = new Resume[size()];
 
         System.arraycopy(storage, 0, allResume, 0, allResume.length);
         return allResume;
     }
 
-    int size() {
+    public int size() {
         return size;
     }
 }
