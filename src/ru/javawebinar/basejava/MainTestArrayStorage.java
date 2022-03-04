7f2234e7706f94c70307ac368f98bc1518ebabc6
engitpank <engutpunk@gmail.com>
2022-03-04 18:04:05 +0300

    Add constructor Resume.java
    - Fixed call Resume in the SortedArrayStorage.java, MainArray.java, MainTestArrayStorage.java

--- src/ru/javawebinar/basejava/MainTestArrayStorage.java
+++ src/ru/javawebinar/basejava/MainTestArrayStorage.java
@@ -12,11 +12,8 @@ public class MainTestArrayStorage {
 
     public static void main(String[] args) {
         Resume r1 = new Resume();
-        r1.setUuid("uuid1");
         Resume r2 = new Resume();
-        r2.setUuid("uuid2");
         Resume r3 = new Resume();
-        r3.setUuid("uuid3");
 
         ARRAY_STORAGE.save(r1);
         ARRAY_STORAGE.save(r2);
@@ -30,7 +27,6 @@ public class MainTestArrayStorage {
         printAll();
 
         Resume r4 = new Resume();
-        r4.setUuid("uuid3");
         ARRAY_STORAGE.update(r4);
         printAll();
 
